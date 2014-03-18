689cd535afe9e095885141492372a3e62727f06f
Joakim Brännström <tok_jocke@hotmail.com>
2014-03-18 22:35:00 +0100

    ???

--- First/sketch1/sketch1.ino
+++ First/sketch1/sketch1.ino
@@ -1,26 +1,31 @@
 
+void toggle(void* args);
+void conditionalAction(bool condition, void (*action)(void*), void* args);
+
 // the setup routine runs once when you press reset:
 void setup() {
   pinMode(0, INPUT);
   pinMode(13, OUTPUT);
 }
 
-void toggle(int pinNr)
+// the loop routine runs over and over again forever:
+void loop() {
+  int pinNr = 13;
+  conditionalAction(digitalRead(0) == 1, toggle, &pinNr);
+}
+
+void toggle(void* args)
 {
+  int pinNr = *((int*)args);
   digitalWrite(pinNr, HIGH);
   delay(100);
   digitalWrite(pinNr, LOW);
   delay(100);
 }
 
-void conditionalAction(bool condition, void (*action)(int), int args)
+void conditionalAction(bool condition, void (*action)(void*), void* args)
 {
   if(condition)
     action(args);
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-  conditionalAction(digitalRead(0) == 1, toggle, 13);
 }
 
